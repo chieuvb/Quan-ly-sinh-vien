dd0a901e3aab8795aaba4b5d2ed4acb2a15d2113
Van Chieu <95832364+chieuvb@users.noreply.github.com>
2023-07-12 08:33:06 +0700

    Delete DataTransfer directory

--- DataTransfer/DTTaikhoan.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DataTransfer
-{
-    public class DTTaikhoan
-    {
-        public string Username { get; set; }
-        public int Password { get; set; }
-        public byte Role { get; set; }
-    }
-}
