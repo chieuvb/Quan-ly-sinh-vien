c8bc0597e08debc977cbb90f4cdaadbf4a41bcf0
Van Chieu <95832364+chieuvb@users.noreply.github.com>
2023-07-12 08:32:51 +0700

    Delete DataAccess directory

--- DataAccess/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataAccess
-{
-    public class Configuration
-    {
-        public static readonly string strCon = @"Data Source=MSI\MSI;Initial Catalog=QuanLyDiem1.0.3;Integrated Security=True";
-    }
-}
